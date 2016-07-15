0ee59f31d212ee3fd90a81be646c48598a7d30dc
AlexanderMykulych <alexander.mykulych@gmail.com>
2016-07-15 09:03:18 +0300

    Init commit

---
+++ Terra-integration/QueryConsole/Files/TsClientServiceInegration.TsBase.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Terrasoft.Core;
+using Terrasoft.Core.Entities;
+using Newtonsoft.Json.Linq;
+using System.Dynamic;
+using System.Data;
+using Terrasoft.Core.Configuration;
+using System.Xml;
+using System.Reflection;
+using System.Threading;
+using System.Web.Configuration;
+
+
+namespace Terrasoft.TsConfiguration
+{
+	using IntegrationInfo = QueryConsole.Files.Constants.CsConstant.IntegrationInfo;
+	using TIntegrationType = QueryConsole.Files.Constants.CsConstant.TIntegrationType;
+	using System.Configuration;
+	using System.Collections;
+	using System.Threading.Tasks;
+	using QueryConsole.Files.BpmEntityHelper;
+	using QueryConsole.Files.Constants;
+
+
+	#region Class: ClientServiceIntegrator
+	
+	#endregion
+
+	#region Static Class: ExtensionHelper
+	public static class ExtensionHelper {
+		#region Methods: Public
+		/// <summary>
+		/// Сериализирует объект
+		/// </summary>
+		/// <param name="obj">Объект</param>
+		/// <returns></returns>
+		public static string SerializeToJson(this object obj) {
+			return Newtonsoft.Json.JsonConvert.SerializeObject(obj).Replace("ReferenceClientService", "#ref");
+		}
+
+		/// <summary>
+		/// Десериализирует объект
+		/// </summary>
+		/// <param name="json">json текс</param>
+		/// <returns></returns>
+		public static Dictionary<string, object> DeserializeJson(this string json) {
+			return Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+		}
+		#endregion
+	}
+	#endregion
+
+	#region Interface: IMappingMethod
+	public interface IMappingMethod {
+		//TODO: Вынести методы маппера в отдельные сущности
+		void Evaluate(MappingItem mappItem, IntegrationInfo integrationInfo);
+	}
+	#endregion
+
+	#region Class: MappingMethodAttribute
+	public class MappingMethodAttribute : System.Attribute {
+		private string methodName;
+		public string MethodName {
+			get {
+				return methodName;
+			}
+		}
+
+		public MappingMethodAttribute(string methodName) {
+			this.methodName = methodName;
+		}
+	}
+	#endregion
+
+	#region Class: IntegrationConfigurationManager
+	public static class IntegrationConfigurationManager {
+		
+		#region Static Fields: Private
+		private static List<string> _columnNames;
+		
+		private static string _xmlData;
+		private static XmlDocument _xDocument;
+		private static MappingItem _defaultItem;
+		private static Dictionary<string, string> _prerenderConfigDict;
+		#endregion
+
+		#region Methods: Private
+		/// <summary>
+		/// Возвращает xml документ c настройками маппинга
+		/// </summary>
+		/// <param name="userConnection"></param>
+		/// <returns></returns>
+		private static XmlDocument GetConfigXmlDocument(UserConnection userConnection) {
+			try {
+				if(_xDocument != null)
+					return _xDocument;
+
+				string confLocation = System.Configuration.ConfigurationManager.AppSettings["XmlConfigurationLocation"] ?? "db";
+				if(confLocation == "db") {
+					if(string.IsNullOrEmpty(_xmlData)) {
+						_xmlData = Terrasoft.Core.Configuration.SysSettings.GetValue(userConnection, CsConstant.SysSettingsCode.ConfigurationData, "<?xml version=\"1.0\" encoding=\"utf-8\"?>");
+					}
+				} else if(confLocation == "file") {
+					if(string.IsNullOrEmpty(_xmlData)) {
+						string confPath = System.Configuration.ConfigurationManager.AppSettings["XmlConfigurationFilePath"] ?? "IntegrationConfig.xml";
+						using(var stream = new StreamReader(confPath)) {
+							_xmlData = stream.ReadToEnd();
+						}
+					}
+				}
+				if(_xDocument == null) {
+					_xDocument = new XmlDocument();
+					_xDocument.LoadXml(_xmlData);
+				}
+				return _xDocument;
+			} catch(Exception e) {
+				//IntegrationLogger.Error("Method [GetConfigXmlDocument] throw exception: Message = {0}", e.Message);
+				throw;
+			}
+		}
+
+		/// <summary>
+		/// Возвращает ноду с именем name, документа doc
+		/// </summary>
+		/// <param name="doc">Документ</param>
+		/// <param name="name">Имя ноды</param>
+		/// <returns></returns>
+		private static XmlNode GetXmlNodeByNameAttr(XmlDocument doc, string name, string tag = null) {
+			try {
+				foreach(XmlNode node in doc.DocumentElement) {
+					if(node is XmlComment)
+						continue;
+					if(
+						(
+							(node.Attributes["TsName"] != null && node.Attributes["TsName"].Value == name) ||
+							(node.Attributes["JName"] != null && node.Attributes["JName"].Value == name)
+						) &&
+						(
+							string.IsNullOrEmpty(tag) || (node.Attributes["Tag"] != null && node.Attributes["Tag"].Value == tag)
+						)
+					) {
+						return node;
+					}
+				}
+				return null;
+			} catch(Exception e) {
+				//IntegrationLogger.Error("Method [GetXmlNodeByNameAttr] throw exception: Message = {0}", e.Message);
+				throw;
+			}
+		}
+
+		/// <summary>
+		/// Возвращает елемент маппинга по ноде из документа конфигурации
+		/// </summary>
+		/// <param name="userConnection"></param>
+		/// <param name="node">Нода</param>
+		/// <param name="defItem">Елемент маппинга по умолчанию. Если в ноде не будет какого-то поля, то подставится поле с этого объекта</param>
+		/// <returns></returns>
+		private static MappingItem GetItemByXmlNode(UserConnection userConnection, XmlNode node, MappingItem defItem = null) {
+			try {
+				var resultObj = Activator.CreateInstance(_mapItemType) as MappingItem;
+				bool isAttrSetting = false;
+				foreach(string attributeName in ColumnNames) {
+					isAttrSetting = false;
+					PropertyInfo propertyInfo = _mapItemType.GetProperty(attributeName);
+					var xmlAttribute = node.Attributes[attributeName];
+					if(xmlAttribute != null) {
+						string xmlValue = PrepareValue(userConnection, xmlAttribute.Value);
+						if(propertyInfo != null) {
+							Type propertyType = propertyInfo.PropertyType;
+
+							if(propertyType.IsEnum || propertyType == typeof(int)) {
+								isAttrSetting = true;
+								propertyInfo.SetValue(resultObj, int.Parse(xmlValue));
+							} else if(propertyType == typeof(bool)) {
+								isAttrSetting = true;
+								propertyInfo.SetValue(resultObj, xmlValue != "0");
+							} else {
+								isAttrSetting = true;
+								propertyInfo.SetValue(resultObj, xmlValue);
+							}
+						}
+					}
+					if(!isAttrSetting && defItem != null) {
+						propertyInfo.SetValue(resultObj, propertyInfo.GetValue(defItem));
+					}
+				}
+				return resultObj;
+			} catch(Exception e) {
+				//IntegrationLogger.Error("Method [GetItemByXmlNode] throw exception: Message = {0}", e.Message);
+				throw;
+			}
+		}
+
+		/// <summary>
+		/// Возращает конфиг пререндеринга prerenderConfig
+		/// </summary>
+		/// <param name="userConnection"></param>
+		/// <returns></returns>
+		private static Dictionary<string, string> GetPrerenderConfig(UserConnection userConnection) {
+			try {
+				if(_prerenderConfigDict != null && _prerenderConfigDict.Any())
+					return _prerenderConfigDict;
+				var doc = GetConfigXmlDocument(userConnection);
+				var element = doc.DocumentElement["prerenderConfig"];
+				_prerenderConfigDict = new Dictionary<string, string>();
+				if(element != null) {
+					foreach(XmlNode confItem in element.ChildNodes) {
+						string from = confItem.Attributes["From"].Value;
+						string to = confItem.Attributes["To"].Value;
+						if(!string.IsNullOrEmpty(from) && !string.IsNullOrEmpty(to)) {
+							_prerenderConfigDict.Add(from, to);
+						}
+					}
+				}
+				return _prerenderConfigDict;
+			} catch(Exception e) {
+				//IntegrationLogger.Error("Method [GetPrerenderConfig] throw exception: Message = {0}", e.Message);
+				throw;
+			}
+		}
+
+		/// <summary>
+		/// Подготавливает конфигурацию маппинга с помощю конфигурации prerenderConfig.
+		/// </summary>
+		/// <param name="userConnection"></param>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		private static string PrepareValue(UserConnection userConnection, string value) {
+			try {
+				var configDict = GetPrerenderConfig(userConnection);
+				if(configDict.ContainsKey(value))
+					return configDict[value];
+				return value;
+			} catch(Exception e) {
+				//IntegrationLogger.Error("Method [PrepareValue] throw exception: Message = {0}", e.Message);
+				throw;
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="userConnection"></param>
+		/// <returns></returns>
+		private static MappingItem GetDefaultItem(UserConnection userConnection) {
+			try {
+				return _defaultItem == null ? _defaultItem = GetItemByXmlNode(userConnection, GetXmlNodeByNameAttr(GetConfigXmlDocument(userConnection), "Default").ChildNodes[0]) : _defaultItem;
+			} catch(Exception e) {
+				//IntegrationLogger.Error("Method [GetDefaultItem] throw exception: Message = {0}", e.Message);
+				throw;
+			}
+		}
+
+		private static MappingItem GetDafaultItemByType(UserConnection userConnection, string type, MappingItem defaultItem = null) {
+			try
+			{
+				var document = GetConfigXmlDocument(userConnection);
+				foreach(XmlNode node in document.DocumentElement) {
+					if(node.Name == "configItemType") {
+						foreach(XmlNode childNode in node.ChildNodes) {
+							if (childNode.Attributes["MapType"] != null && childNode.Attributes["MapType"].Value == type)
+							{
+								return GetItemByXmlNode(userConnection, childNode, defaultItem);
+							}
+						}
+					} 
+				}
+				return defaultItem;
+			}
+			catch (Exception e)
+			{
+				//IntegrationLogger.Error("Method [GetDafaultItemByType] throw exception: Message = {0}", e.Message);
+				throw;
+			}
+		}
+
+		#endregion
+
+		#region Methods: Public
+		public static List<MappingItem> GetConfigItem(UserConnection userConnection, string Name, string tag = null) {
+			try {
+				var result = new List<MappingItem>();
+				XmlDocument xDoc = GetConfigXmlDocument(userConnection);
+				var node = GetXmlNodeByNameAttr(xDoc, Name, tag);
+				
+				var defItem = GetDefaultItem(userConnection);
+				foreach(XmlNode mapItem in node.ChildNodes) {
+					if(mapItem is XmlElement) {
+						string type = null;
+						if (mapItem.Attributes["MapType"] != null)
+						{
+							type = mapItem.Attributes["MapType"].Value;
+						} else {
+							type = defItem.MapType.ToString();
+						}
+						var typeDefItem = GetDafaultItemByType(userConnection, type, defItem);
+						result.Add(GetItemByXmlNode(userConnection, mapItem, typeDefItem));
+					}
+				}
+				return result;
+			} catch(Exception e) {
+				//IntegrationLogger.Error("Method [GetConfigItem] throw exception: Message = {0}", e.Message);
+				throw;
+			}
+		}
+		#endregion
+
+		#region Static Fields: Public
+		public static Type _mapItemType = typeof(MappingItem);
+		#endregion
+
+		#region Properties: Public
+		public static List<string> ColumnNames {
+			get {
+				return _columnNames == null || !_columnNames.Any() ? _columnNames = _mapItemType.GetProperties().Where(x => x.MemberType == MemberTypes.Property).Select(x => x.Name).ToList() : _columnNames;
+			}
+		}
+		#endregion
+		
+	}
+	#endregion
+}
---
+++ Terra-integration/QueryConsole/Program.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Reflection;
+using Terrasoft.Common;
+using Terrasoft.TsConfiguration;
+using Terrasoft.Core;
+using Terrasoft.Core.DB;
+using Terrasoft.Core.Entities;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
+using QueryConsole.Files.Integrators;
+using QueryConsole.Files.BpmEntityHelper;
+using QueryConsole.Files.Constants;
+using QueryConsole.Files.IntegratorTester;
+
+namespace QueryConsole
+{
+	public class Program
+	{
+		public static void Main(string[] args) {
+			try {
+				//var consoleApp = new TerrasoftConsoleClass("A.Mykulych");
+				var consoleApp = new TerrasoftConsoleClass("Default");
+				try {
+					consoleApp.Run();
+				} catch(Exception e) {
+					consoleApp.ConsoleColorWrite("Connect to Database: Failed", ConsoleColor.Red);
+					Console.WriteLine(e.Message);
+				}
+				consoleApp.ConsoleColorWrite("Connect to Database: Success");
+				Console.WriteLine("Press any button to start integrate");
+				Console.ReadKey();
+				var testers = new List<BaseIntegratorTester>() {
+					new OrderServiceIntegratorTester(consoleApp.SystemUserConnection),
+					new ClientServiceIntegratorTester(consoleApp.SystemUserConnection)
+				};
+				//	//Account
+
+				//	var esq = new EntitySchemaQuery(consoleApp.SystemUserConnection.EntitySchemaManager, "Account");
+				//	esq.AddAllSchemaColumns();
+				//	esq.RowCount = 1;
+				//	var createdOn = esq.AddColumn("CreatedOn");
+				//	createdOn.OrderByDesc();
+				//	var entity = esq.GetEntityCollection(consoleApp.SystemUserConnection)[0];
+				//	testers[1].ImportBpmEntity(entity);
+				////Contact
+
+				//	var esq2 = new EntitySchemaQuery(consoleApp.SystemUserConnection.EntitySchemaManager, "Contact");
+				//	esq2.AddAllSchemaColumns();
+				//	esq2.RowCount = 1;
+				//	var createdOn2 = esq2.AddColumn("CreatedOn");
+				//	createdOn2.OrderByDesc();
+				//	var entity2 = esq2.GetEntityCollection(consoleApp.SystemUserConnection)[0];
+				//	testers[1].ImportBpmEntity(entity2);
+				////TsAutomobile
+				//	var esq3 = new EntitySchemaQuery(consoleApp.SystemUserConnection.EntitySchemaManager, "TsAutomobile");
+				//	esq3.AddAllSchemaColumns();
+				//	esq3.RowCount = 1;
+				//	var createdOn3 = esq3.AddColumn("CreatedOn");
+				//	createdOn3.OrderByDesc();
+				//	var entity3 = esq3.GetEntityCollection(consoleApp.SystemUserConnection)[0];
+				//	testers[1].ImportBpmEntity(entity3);
+				//	int limit = 10;
+				//	limit = int.Parse(Console.ReadLine());
+				//testers[0].Limit = 50;
+				//testers[0].Skip = 0;
+				//testers[0].ExportServiceEntity("CounteragentContactInfo", () =>
+				//{
+
+				//		testers[0].Limit = 1000;
+				//		testers[0].Skip = 2000;
+				//		testers[0].ExportServiceEntity("Counteragent", () =>
+				//		{
+				//			testers[0].Limit = 1000;
+				//			testers[0].Skip = 3000;
+				//			testers[0].ExportServiceEntity("Counteragent", () =>
+				//			{
+				//				testers[0].Limit = 1000;
+				//				testers[0].Skip = 4000;
+				//				testers[0].ExportServiceEntity("Counteragent", () =>
+				//				{
+				//					testers[0].Limit = 1000;
+				//					testers[0].Skip = 5000;
+				//					testers[0].ExportServiceEntity("Counteragent", () =>
+				//					{
+				//						testers[0].Limit = 1000;
+				//						testers[0].Skip = 6000;
+				//						testers[0].ExportServiceEntity("Counteragent", () =>
+				//						{
+											
+				//									testers[0].Limit = 1000;
+				//									testers[0].Skip = 0;
+				//									testers[0].ExportServiceEntity("Contract", () =>
+				//									{
+				//										testers[0].Limit = 1000;
+				//										testers[0].Skip = 1000;
+				//										testers[0].ExportServiceEntity("Contract", () =>
+				//										{
+				//											testers[0].Limit = 1000;
+				//											testers[0].Skip = 2000;
+				//											testers[0].ExportServiceEntity("Contract", () =>
+				//											{
+				//												testers[0].Limit = 1000;
+				//												testers[0].Skip = 3000;
+				//												testers[0].ExportServiceEntity("Contract", () =>
+				//												{
+				//													testers[0].Limit = 1000;
+				//													testers[0].Skip = 4000;
+				//													testers[0].ExportServiceEntity("Contract", () =>
+				//													{
+				//														testers[0].Limit = 1000;
+				//														testers[0].Skip = 5000;
+				//														testers[0].ExportServiceEntity("Contract", () =>
+				//														{
+				//															testers[0].Limit = 1000;
+				//															testers[0].Skip = 6000;
+				//															testers[0].ExportServiceEntity("Contract", () =>
+				//															{
+																				testers[0].Limit = 500;
+																				testers[0].Skip = 0;
+																				testers[0].ExportServiceEntity("Order", () =>
+																				{
+																					testers[0].Limit = 1000;
+																					testers[0].Skip = 500;
+																					testers[0].ExportServiceEntity("Order", () =>
+																					{
+																						testers[0].Limit = 1500;
+																						testers[0].Skip = 0;
+																						testers[0].ExportServiceEntity("Shipment", () =>
+																						{
+																							testers[0].Limit = 1500;
+																							testers[0].Skip = 0;
+																							testers[0].ExportServiceEntity("Payment", () =>
+																							{
+																								testers[0].Limit = 1500;
+																								testers[0].Skip = 0;
+																								testers[0].ExportServiceEntity("Return", () =>
+																								{
+
+																								});
+																							});
+																						});
+																					});
+																				});
+				//															});
+				//														});
+
+				//													});
+															
+				//										});
+				//									});
+				//								});
+				//							});
+				//						});
+				//				});
+				//			});
+				//		});
+				//	});
+				//});
+
+				
+				//var test3 = new IntegrationServiceIntegrator(consoleApp.SystemUserConnection);
+				//test3.GetBusEventNotification(true);
+				//tester.ExportAllServiceEntities(1000);
+				//tester.ExportAllServiceEntitiesByStep(20, 3500);
+				//
+				//tester2.ExportAllServiceEntities(100);
+				//tester.ImportAllBpmEntity();
+				while (true) {
+				}
+			} catch (ReflectionTypeLoadException e1) {
+				Console.WriteLine(e1.Message);
+			} catch (Exception e) {
+				Console.WriteLine(e.Message);
+			}
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("Press eny key!");
+			Console.ReadKey();
+		}
+	}
+
+	#region Class: TerrasoftConsoleClass
+
+	public class TerrasoftConsoleClass
+	{
+		#region Constructors: Public
+
+		public TerrasoftConsoleClass(string workspaceName) {
+			WorkspaceName = workspaceName;
+			SystemUserConnection = AppConnection.SystemUserConnection;
+			AppManagerProvider
+			= _appManagerProvider ?? (_appManagerProvider = AppConnection.AppManagerProvider);
+		}
+
+		#endregion
+
+		#region Properties: Public
+
+		public string WorkspaceName {
+			get;
+			set;
+		}
+
+		public UserConnection SystemUserConnection;
+
+		private AppConnection _appConnection;
+
+		public AppConnection AppConnection {
+			get {
+				if (_appConnection == null) {
+					_appConnection = new AppConnection();
+				}
+				return _appConnection;
+			}
+			protected set {
+				_appConnection = value;
+			}
+		}
+
+		private ManagerProvider _appManagerProvider;
+
+		public ManagerProvider AppManagerProvider;
+
+		#endregion
+
+		#region Methods: Protected
+
+		protected virtual Assembly CurrentDomainAssemblyResolve(object sender, ResolveEventArgs args) {
+			string requestingAssemblyName = args.Name;
+			var appUri = new UriBuilder(Assembly.GetExecutingAssembly().CodeBase);
+			string host = appUri.Host;
+			if (!string.IsNullOrEmpty(host)) {
+				host = @"\\" + host;
+			}
+			string appPath = Path.Combine(host, Path.GetDirectoryName(Uri.UnescapeDataString(appUri.Path.TrimStart('/'))));
+			var processRunMode = Environment.Is64BitProcess ? "x64" : "x86";
+			int index = requestingAssemblyName.IndexOf(',');
+			if (index > 0) {
+				string requestingAssemblyPath = Path.Combine(appPath, processRunMode,
+					requestingAssemblyName.Substring(0, index) + ".dll");
+				if (System.IO.File.Exists(requestingAssemblyPath)) {
+					return Assembly.LoadFrom(requestingAssemblyPath);
+				}
+			}
+			return null;
+		}
+
+		protected AppConfigurationSectionGroup GetAppSettings() {
+			Configuration configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+			var appSettings = (AppConfigurationSectionGroup) configuration.SectionGroups["terrasoft"];
+			appSettings.RootConfiguration = configuration;
+			return appSettings;
+		}
+
+		protected virtual void Initialize(ConfigurationSectionGroup appConfigurationSectionGroup) {
+			var appSettings = (AppConfigurationSectionGroup) appConfigurationSectionGroup;
+			string appDirectory = Path.GetDirectoryName(this.GetType().Assembly.Location);
+			appSettings.Initialize(appDirectory, Path.Combine(appDirectory, "App_Data"), Path.Combine(appDirectory, "Resources"),
+				appDirectory);
+			AppConnection.Initialize(appSettings);
+			AppConnection.InitializeWorkspace(WorkspaceName);
+		}
+
+		#endregion
+
+		#region Methods: Public
+
+		public void Run() {
+			AppDomain.CurrentDomain.AssemblyResolve += CurrentDomainAssemblyResolve;
+			AppConfigurationSectionGroup appSettings = GetAppSettings();
+			var resources = (ResourceConfigurationSectionGroup) appSettings.SectionGroups["resources"];
+			GeneralResourceStorage.Initialize(resources);
+			Initialize(appSettings);
+		}		
+		public EntityCollection GetEntitiesForUpdate(string name, bool onlyNotImportet = false) {
+			var esq = new EntitySchemaQuery(SystemUserConnection.EntitySchemaManager, name);
+			esq.AddAllSchemaColumns();
+			if(onlyNotImportet) {
+				//esq.Filters.Add(esq.CreateFilterWithParameters(FilterComparisonType.Equal, "TsExternalId", 0));
+			}
+			return esq.GetEntityCollection(SystemUserConnection);
+		}
+
+		public void UpdateEntity(string entityName) {
+			ConsoleColorWrite(string.Format("{0}: Start Integrate", entityName));
+			var entities = GetEntitiesForUpdate(entityName);
+			int j = 1, count = entities.Count;
+			ConsoleColorWrite(string.Format("{0} count: {1}", entityName, count));
+			foreach(var entity in entities) {
+				Console.WriteLine("{0} with id = '{1}'", entityName, entity.PrimaryColumnValue.ToString());
+				var integrator = new ClientServiceIntegrator(SystemUserConnection);
+				var sw = new Stopwatch();
+				sw.Start();
+				integrator.Update(entity);
+				sw.Stop();
+				ConsoleColorWrite("sec = " + sw.ElapsedMilliseconds / 1000.0 + " ");
+				Console.WriteLine(j++ + "/" + count);
+			}
+			ConsoleColorWrite(entityName + ": end integrate");
+		}
+		public void Import(string json, string entityName) {
+			var integrator = new IntegrationEntityHelper();
+			var integrationInfo = new CsConstant.IntegrationInfo(Newtonsoft.Json.JsonConvert.DeserializeObject(json) as JObject, SystemUserConnection, CsConstant.TIntegrationType.Import, null, entityName, "create");
+			var sw = new Stopwatch();
+			sw.Start();
+			integrator.IntegrateEntity(integrationInfo);
+			sw.Stop();
+			Console.WriteLine("sec = " + sw.ElapsedMilliseconds / 1000.0);
+			Console.WriteLine("millisec = " + sw.ElapsedMilliseconds);
+			if(integrationInfo.Result != null && integrationInfo.Result.Type == CsConstant.IntegrationResult.TResultType.Success) {
+				Console.ForegroundColor = ConsoleColor.Green;
+				Console.WriteLine("Success");
+			}
+		}
+
+		public void ConsoleColorWrite(string text, ConsoleColor color = ConsoleColor.Green) {
+			var buff = Console.ForegroundColor;
+			Console.ForegroundColor = color;
+			Console.WriteLine(text);
+			Console.ForegroundColor = buff;
+		}
+		#endregion
+	}
+
+	#endregion
+
+}
